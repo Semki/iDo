2f98125c6a6fc267d939853acfd0c44174e88d24
de1mos <de1m0s242@gmail.com>
2012-03-31 18:01:14 +0800

    data layer

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,2 @@
 Activity.create([{:name => 'Eating'}, {:name => 'Coding'}, {:name => 'Working'}, {:name => 'Running'}, {:name => 'Fucking'}, {:name => 'Pissing'}, {:name => 'Sleeping'} ])
+Globals.clean
