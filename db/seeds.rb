92ec0632c7ce7df8e7c66809f2dd3d075d19e3bd
Dennis Kerner <denkern@gmail.com>
2012-03-31 19:07:52 +0800

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 Activity.destroy_all
 Activity.create([{:name => 'Eating'}, {:name => 'Coding'}, {:name => 'Working'}, {:name => 'Running'}, {:name => 'Fucking'}, {:name => 'Pissing'}, {:name => 'Sleeping'} ])
+Achievement.destroy_all
 Achievement.create([{:name => 'fat man', :duration => '1', :count => '5' }, {:name => 'runner of first level', :duration => '100', :count => '5' }])
 Globals.clean
