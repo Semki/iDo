3c6464c2c257b826941cf4a54f53d91f179f1f24
antoshalee <antoshalee@gmail.com>
2012-03-31 18:34:59 +0800

    rename migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120331094102) do
+ActiveRecord::Schema.define(:version => 20120331094500) do
 
   create_table "achievements", :force => true do |t|
     t.string   "name"
