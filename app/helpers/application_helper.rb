adc23e5334399b5c9063507fe6f3f615812e3aa2
Anton Zhavoronkov <djzhan@list.ru>
2012-03-31 15:24:59 +0800

    work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def authorisation_html
+    logged_in = true
+    username = "Kvitunov"
+
+    if logged_in
+      "Logged in as <b>Kvitunov</b>.<a>Log out</a>".html_safe
+    else
+      "You are not signed in. <a>Sign up</a> or <a>Sign in</a>".html_safe
+    end
+  end
 end
