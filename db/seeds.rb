e6243d0798fd821e9c4869629aae80d11c3bd114
de1mos <de1m0s242@gmail.com>
2012-03-31 19:02:11 +0800

    Drop activity before seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
+Activity.destroy_all
 Activity.create([{:name => 'Eating'}, {:name => 'Coding'}, {:name => 'Working'}, {:name => 'Running'}, {:name => 'Fucking'}, {:name => 'Pissing'}, {:name => 'Sleeping'} ])
 Globals.clean
