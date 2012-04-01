0fee5f7c4bbb0afc00342f3d47876e86ecad2454
Anton Zhavoronkov <djzhan@list.ru>
2012-04-01 18:38:27 +0800

    1

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -10,6 +10,13 @@ module ActivitiesHelper
     }
   end
 
+  def users_doing_the_same(activity_id)
+    Globals.get_users_doing_the_same(activity_id).collect{|ra|
+      {:user_id => ra, :user_name => User.find(ra).name,
+       :user_picture => User.find(ra).image_url, :activity_id => activity_id }
+    }
+  end
+
   def activity_of_current_user
     Globals.get_user_current_activity(current_user.id)
   end
