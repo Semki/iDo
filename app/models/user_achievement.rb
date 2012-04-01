28b5e8626ba9d936c9cf56090c504d718e2a82ae
Dennis Kerner <denkern@gmail.com>
2012-04-01 19:11:53 +0800

    delete

--- app/models/user_achievement.rb
+++
@@ -1,8 +0,0 @@
-class UserAchievement < ActiveRecord::Base
-  attr_accessible :achievement_id, :user_id
-  
-  belongs_to :achievement, :class_name=>'Achievement'
-  belongs_to :user, :class_name=>'User'
-  
-  
-end
--- app/models/user_bonus.rb
+++
@@ -1,7 +0,0 @@
-class UserBonus < ActiveRecord::Base
-  attr_accessible :bonus_id, :user_id
-  
-  def self.find_by_user_and_bonus(user_id, bonus_id)
-    UserBonus.where(:user_id => user_id, :bonus_id => bonus_id)
-  end
-end
