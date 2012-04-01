66bc08e5cfde98102e834d4b07cb559258610900
Dennis Kerner <denkern@gmail.com>
2012-04-01 15:33:41 +0800

    pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ activity_working = Activity.create({:name => 'Working', :picture_url => 'working
 activity_cleaning = Activity.create({:name => 'Cleaning', :picture_url => 'cleaning.png'})
 activity_shopping = Activity.create({:name => 'Shopping', :picture_url => 'shopping.png'})
 activity_running = Activity.create({:name => 'Running', :picture_url => 'running.png'})
-Achievement.create({:name => 'runner of first level', :duration => '100', :count => '25', :activity_id => activity_running.id, :picture_url => 'runner.png' })
+Achievement.create({:name => 'runner of first level', :duration => '100', :count => '25', :activity_id => activity_running.id, :picture_url => 'running.png' })
 
 #activity_pissing = Activity.create({:name => 'Pissing', :picture_url => ''})
 #activity_sleeping = Activity.create({:name => 'Sleeping', :picture_url => ''})
@@ -17,9 +17,9 @@ activity_studying = Activity.create({:name => 'Studying', :picture_url => 'study
 
 Achievement.create({:name => 'nerd', :duration => '10', :count => '20', :activity_id => activity_studying.id , :picture_url => 'nerd.png'})
 
-activity_walking = Activity.create({:name => 'Walking with dog', :picture_url => 'dog_walking.png'})
+activity_walking = Activity.create({:name => 'Walking with dog', :picture_url => 'walking_with_dog.png'})
 activity_drinking = Activity.create({:name => 'Drinking', :picture_url => 'drinking.png'})
-activity_playing_balalaika = Activity.create({:name => 'Playing balalaika with my bear', :picture_url => 'music.png'})
+activity_playing_balalaika = Activity.create({:name => 'Playing balalaika with my bear', :picture_url => 'playing_balalaika_with_my_bear.png'})
 Achievement.create({:name => 'balalaika perfomance', :duration => '0', :count => '1', :activity_id => activity_playing_balalaika.id, :picture_url => 'balalaika.png' })
 
 UserAchievement.destroy_all
